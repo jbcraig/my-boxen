ecb7397ada05879e8931ed8804c30c90c893b3c9
Jonathan Craig <jon.craig@gfs.com>
2016-03-25 18:48:06 -0400

    Cleaned up atom::packages and unneeded apps

--- modules/people/manifests/jbcraig/applications.pp
+++ modules/people/manifests/jbcraig/applications.pp
@@ -1,34 +1,30 @@
 class people::jbcraig::applications {
   include atom
-  ## XXX the following is extremely slow, should try
-  ## adding it as a list passed to the atom class or
-  ## use a spaceship to collect them up ala the
-  ## homebrew_packages
-  # atom::package { 'aligner': }
-  # atom::package { 'aligner-puppet': }
-  # atom::package { 'aligner-python': }
-  # atom::package { 'atom-beautify': }
-  # atom::package { 'atom-overtype-mode': }
-  # atom::package { 'atom-yamljson': }
-  # atom::package { 'autocomplete-python': }
-  # atom::package { 'export-html': }
-  # atom::package { 'git-time-machine': }
-  # atom::package { 'hex': }
-  # atom::package { 'language-puppet': }
-  # atom::package { 'linter': }
-  # atom::package { 'linter-js-yaml': }
-  # atom::package { 'linter-puppet-lint': }
-  # atom::package { 'linter-puppet-parse': }
-  # atom::package { 'markdown-writer': }
-  # atom::package { 'merge-conflicts': }
-  # atom::package { 'minimap': }
-  # atom::package { 'minimap-cursorline': }
-  # atom::package { 'minimap-find-and-replace': }
-  # atom::package { 'minimap-linter': }
-  # atom::package { 'minimap-split-diff': }
-  # atom::package { 'project-manager': }
-  # atom::package { 'split-diff': }
-  # atom::package { 'vim-mode': }
+  atom::package { 'aligner': }
+  atom::package { 'aligner-puppet': }
+  atom::package { 'aligner-python': }
+  atom::package { 'atom-beautify': }
+  atom::package { 'atom-overtype-mode': }
+  atom::package { 'atom-yamljson': }
+  atom::package { 'autocomplete-python': }
+  atom::package { 'export-html': }
+  atom::package { 'git-time-machine': }
+  atom::package { 'hex': }
+  atom::package { 'language-puppet': }
+  atom::package { 'linter': }
+  atom::package { 'linter-js-yaml': }
+  atom::package { 'linter-puppet-lint': }
+  atom::package { 'linter-puppet-parse': }
+  atom::package { 'markdown-writer': }
+  atom::package { 'merge-conflicts': }
+  atom::package { 'minimap': }
+  atom::package { 'minimap-cursorline': }
+  atom::package { 'minimap-find-and-replace': }
+  atom::package { 'minimap-linter': }
+  atom::package { 'minimap-split-diff': }
+  atom::package { 'project-manager': }
+  atom::package { 'split-diff': }
+  atom::package { 'vim-mode': }
 
   # include virtualbox
   # include tunnelblick
@@ -82,16 +78,16 @@ class people::jbcraig::applications {
 # }
 
 # Libre FTP, SFTP, WebDAV, S3, Azure & OpenStack Swift browser for Mac and Windows
-package { 'Cyberduck':
-  source   => 'http://c315635.r35.cf1.rackcdn.com/Cyberduck-4.2.1.zip',
-  provider => compressed_app
-}
+# package { 'Cyberduck':
+#   source   => 'http://c315635.r35.cf1.rackcdn.com/Cyberduck-4.2.1.zip',
+#   provider => compressed_app
+# }
 
 # An advanced Mac system monitor (license controlled)
-package { 'iStat Menus':
-  source   => 'http://s3.amazonaws.com/bjango/files/istatmenus3/istatmenus3.27.zip',
-  provider => compressed_app
-}
+# package { 'iStat Menus':
+#   source   => 'http://s3.amazonaws.com/bjango/files/istatmenus3/istatmenus3.27.zip',
+#   provider => compressed_app
+# }
 
 # package { 'Github':
 #   source   => 'https://github-central.s3.amazonaws.com/mac%2FGitHub%20for%20Mac%20124.zip',
